bc3f34921c858a54ec99dad8def4b8f10d080c9a
ikrysteq <ikrysteq@gmail.com>
2017-10-11 11:33:16 +0200

    added folder Pipes

--- sortNumber.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SortNumberPipe } from './sortNumber.pipe';
-
-describe('SortNumberPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SortNumberPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- sortString.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SortStringPipe } from './sortString.pipe';
-
-describe('SortStringPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SortStringPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
